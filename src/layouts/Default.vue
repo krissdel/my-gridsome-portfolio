7a00c66515596c96a0267192c56241344baca6db
christian <delormechrist@gmail.com>
2021-01-28 11:26:57 +0100

    update index.js

---
+++ src/layouts/Default.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-navigation-drawer width="300" permanent>
+      <v-list-item>
+<!-- <v-avatar> -->
+      <!-- <img
+        src="https://images/moi.jpg"
+        alt="John"
+      >
+    </v-avatar> -->
+
+        <v-list-item-content>
+          <v-list-item-title class="title"> CHRISTIAN DELORME </v-list-item-title>
+          <v-list-item-subtitle> Full stack developer </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <slot/>
+    </v-main>
+  </v-app>
+</template>
+  
+
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "About", icon: "mdi-account-box-outline" },
+        { title: "Projects", icon: "mdi-apps" },
+        { title: "Education", icon: "mdi-school-outline" },
+        { title: "Contact", icon: "mdi-message-text-outline" },
+],
+    };
+  },
+};
+</script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+ .v-application--wrap {
+   flex-direction: row;
+ }
+ .v-main {
+   flex: 1 0;
+ }
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <Layout>
+    <section>
+      <v-container>
+         <v-row>
+            <v-col>
+                <g-image alt="ma photo de profile" src="~/moi.png" width="300" />
+                
+            </v-col>
+            <v-col>
+              <!-- <div class="d-flex flex-column"> -->
+
+                <h1>Hi!  </h1>
+                <h2>i'm christ</h2>
+                <p> i'm a full stack dev </p>
+                <!-- </div> -->
+            </v-col>
+         </v-row>
+      </v-container>  
+</section>
+
+   
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
